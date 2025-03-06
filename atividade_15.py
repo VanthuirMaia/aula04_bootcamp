2a3ce76466ee526a1f09df2610dd8e81e5b74d6d
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-05 23:01:28 -0300

    feat - Aula 04 -  Atividade 15

---
+++ atividade_15.py
@@ -0,0 +1,14 @@
+# Objetivo: Dada uma string, contar a frequência de cada caractere usando um dicionário.
+
+texto = input("Digite sua String: ")
+
+frequencia = {}
+
+for caractere in texto:
+    frequencia[caractere] = frequencia.get(caractere, 0) + 1
+
+frequencia_ordenada = dict(sorted(frequencia.items(), key=lambda item: item[1], reverse=True))
+
+print(frequencia)
+print(frequencia_ordenada)
+
