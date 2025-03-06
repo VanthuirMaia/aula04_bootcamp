6a5366af4cdcfcdeeff488c6899be2bdfbd9f8dc
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-05 21:14:01 -0300

    feat - Aula 04 - Leitor de CSV

---
+++ aula_algoritmo.py
@@ -0,0 +1,16 @@
+lista_de_numeros: list = [40,50,60,70,0,-408593,1,50]
+
+# [40,50,960,70,0,-408593,1,50]
+# [50,60,,700,-408593,1,50]
+
+def ordernar_lista_de_numeros(numeros: list) -> list:
+    nova_lista_de_numeros = numeros.copy()
+    for i in range(len(nova_lista_de_numeros)):
+        for j in range(i+1, len(lista_de_numeros)):
+            if nova_lista_de_numeros[i] > nova_lista_de_numeros[j]:
+                nova_lista_de_numeros[i], nova_lista_de_numeros[j] = nova_lista_de_numeros[j], nova_lista_de_numeros[i]
+
+    return nova_lista_de_numeros
+
+nova_lista = ordernar_lista_de_numeros(lista_de_numeros)
+print(nova_lista)
