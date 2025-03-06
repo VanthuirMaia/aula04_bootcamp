8dcbdb89610c869c779826c6d236a5dd838bfeb2
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-05 23:32:32 -0300

    feat - Aula 04 -  Atividade 18

---
+++ atividade_18.py
@@ -0,0 +1,9 @@
+# Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
+
+def reverter(p):
+    return palavra[::-1]
+
+palavra = input("Digite a palavra: ")
+resultado = reverter(palavra)
+
+print(resultado)
