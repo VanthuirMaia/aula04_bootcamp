37b320f0db9f0a19ef911ab5c9901d2497feec69
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-05 22:21:14 -0300

    feat - Aula 04 -  Atividade 07

--- atividade_07.py
+++ atividade_07.py
@@ -1,6 +1,6 @@
 # Objetivo: Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
 
-idades = [22, 15, 30, 17, 18]
+idades:list = [22, 15, 30, 17, 18]
 
 maior_idade:list = []
 
@@ -13,3 +13,10 @@ maior_idade_ordenada = sorted(maior_idade)
 print(maior_idade)
 print(maior_idade_ordenada)
 
+
+
+#Opção da Jornada de Dados 
+idades = [22, 15, 30, 17, 18]
+idades_validas = [idade for idade in idades if idade >= 18]
+
+print(idades_validas)
